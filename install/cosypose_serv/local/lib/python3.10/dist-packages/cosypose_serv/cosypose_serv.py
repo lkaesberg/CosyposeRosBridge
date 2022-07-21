721bb32ff85e0fd2f195ed5d830ce0edb9fc11bf
lars <larskaesberg@yahoo.de>
2022-07-21 12:22:13 +0200

    init()

---
+++ install/cosypose_serv/local/lib/python3.10/dist-packages/cosypose_serv/cosypose_serv.py
@@ -0,0 +1,43 @@
+import rclpy
+from rclpy.node import Node
+
+import rospy
+import roslib
+
+import numpy as np
+
+from sensor_msgs.msg import CompressedImage
+
+
+class MinimalSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            CompressedImage,
+            'cam_right/image_color/compressed',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        print(np.array(msg.data))
+        print()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    minimal_subscriber = MinimalSubscriber()
+
+    rclpy.spin(minimal_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
